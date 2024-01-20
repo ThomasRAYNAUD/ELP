a0b59d5dddf7d1a4d2c5ea323e5e1a01df5351d5
ThomasRAYNAUD <toto.raynaud@sfr.fr>
2024-01-20 15:13:07 +0100

    push du scan

--- GO/open_port/open.go
+++ GO/open_port/open.go
@@ -3,33 +3,23 @@ package main
 import (
 	"fmt"
 	"net"
-	"sync"
 )
-
-var wg sync.WaitGroup
-
-
-func handleConnection(conn net.Conn) {
-	defer conn.Close()
-}
 
 func main() {
 	port:="20001"
 	listener, err := net.Listen("tcp", ":"+port)
+	//créer un listener sur un port TCP spécifié
 	if err != nil {
 		fmt.Printf("Erreur lors de l'écoute sur le port %s, le message d'erreur --> %v\n", port, err)
 		return
 	}
 	fmt.Printf("Serveur en attente de connexions sur le port %s...\n", port)
-
-	for {
-		conn, err := listener.Accept()
+	for { //équivalent d'un while true qui n'existe pas en go
+		_, err := listener.Accept() //accept la connection qd qlq
 		if err != nil {
 			fmt.Println("Erreur lors de l'acceptation de la connexion:", err)
 			continue
 		}
 		fmt.Println("Nouvelle connexion établie.")
-		go handleConnection(conn)
 	}
 }
-
--- GO/open_port/web_server.go
+++ GO/open_port/web_server.go
@@ -5,21 +5,18 @@ import (
 	"html/template"
 )
 
-type Page struct {
-	Valeur string
-}
-
-var templates = template.Must(template.ParseFiles("index.html"))
+var templates = template.Must(template.ParseFiles("index.html")) 
+//charge le fichier "index.html" comme modèle
+// stocke le modèle prêt à être utilisé dans la variable templates
 
 func main() {
-	http.HandleFunc("/home", homeHandler)
-	http.ListenAndServe(":9999", nil)
+	http.HandleFunc("/home", homeHandler) // correspondance entre l'URL "/home" et la fonction homeHandler -> homeHandler sera executé quand qlq accède à l'URL
+	http.ListenAndServe(":9999", nil) // démarre un serveur web HTTP sur le port 9999
 }
 
-func homeHandler(w http.ResponseWriter, r *http.Request) {
-	p := Page{Valeur: "mon premier essai"}
-	err := templates.ExecuteTemplate(w, "index.html", p)
-	if err != nil {
-		http.Error(w, err.Error(), http.StatusInternalServerError)
-	}
+func homeHandler(w http.ResponseWriter, r *http.Request) { //w = définit comment écrire une réponse HTTP
+    err := templates.ExecuteTemplate(w, "index.html", nil) //applique et transmet la template en http -> sans trasmettre de valeur
+    if err != nil { // si erreur retourne l'erreur
+        http.Error(w, err.Error(), http.StatusInternalServerError)
+    }
 }
